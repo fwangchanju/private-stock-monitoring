bbb6b149367a62ae5ee29be5dadf5c24b958f3ae
얼매 <fwangchanju1@gmail.com>
2026-03-13 16:10:12 +0900

    project init

---
+++ src/main/java/dev/eolmae/psms/PrivateStockMonitoringApplication.java
@@ -0,0 +1,13 @@
+package dev.eolmae.psms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateStockMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateStockMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/eolmae/psms/PrivateStockMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.eolmae.psms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateStockMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
